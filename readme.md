bd6a83f40293d37de065f3037431b10ed32a58fa
davidcallizaya <davidcallizaya@192.168.10.102>
2017-03-12 09:30:18 -0400

    my git hook test

---
+++ readme.md
@@ -0,0 +1,3 @@
+# My githook
+
+updates automatically some projects and branches on the server
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$app->get('/deploy/{project}', function ($project, \Illuminate\Http\Request $request) use ($app) {
+    dd($request->json());
+    return $project;
+});
