63bba5e36061e57bef889e513dcd71d5fc6b33e6
davidcallizaya <davidcallizaya@192.168.10.102>
2017-03-12 16:05:07 -0400

    my git hook test

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,34 @@
 <?php
-
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It is a breeze. Simply tell Lumen the URIs it should respond to
+  | and give it the Closure to call when that URI is requested.
+  |
+ */
 
 $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-$app->post('/deploy/{project}', function ($project, \Illuminate\Http\Request $request) use ($app) {
-    dd([
-        realpath(base_path().'/..'),
-        @$request->json()->get('repository')['default_branch']
-    ]);
-    return $project;
-});
+$app->post(
+    '/deploy/{projects}',
+    function ($projects, \Illuminate\Http\Request $request) use ($app) {
+        $response = [];
+        $base = realpath(base_path().'/..');
+        $branch = @$request->json()->get('repository')['default_branch'];
+        foreach (explode(',', $projects) as $project) {
+            $path = $base.'/'.$project;
+            chdir($path);
+            $res = shell_exec('git pull');
+            $response[] = [
+                "name" => $project,
+                "response" => $res,
+            ];
+        }
+        return $response;
+    }
+);
