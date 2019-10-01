291e85a8ad6372fa01e50f958a70993155737c0d
David Callizaya <davidcallizaya@gmail.com>
2019-10-01 11:07:44 -0400

    test

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ $app->post(
         global $lastCommit;
         global $logFile;
         $payload = json_decode($request->input('payload'));
+        var_dump($payload);die;
         $lastCommit = $payload->before;
         $logFile = uniqid() . '.txt';
         if (!getenv('HOME')) {
